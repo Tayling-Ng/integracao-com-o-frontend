bcfdd9c70e215a035569419fd0a024ecc7cdaa29
Tayling Ng <taymng@yahoo.com.br>
2024-05-25 01:10:54 +0000

    Implementação de erros inesperados

--- src/index.js
+++ src/index.js
@@ -23,10 +23,15 @@ async function main() {
     // Rotas
     app.use('/personagem', personagemRouter)
 
+    // Error Handling
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.status(500).send({ error: 'Algo deu errado!' });
+    });
 
-app.listen(3000, function() {
-    console.log("Servidor rodando em http://localhost:3000")
-})
+    app.listen(3000, function() {
+        console.log("Servidor rodando em http://localhost:3000")
+    })
 
 }
 
